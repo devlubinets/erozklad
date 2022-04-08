8ce266efb9ae014f335a91a9eba7a6e300536786
ad <dev.lubinets@gmail.com>
2022-04-08 21:59:16 +0300

    feat(db):add db migration

---
+++ database/migrations/2022_04_08_092813_create_institutions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateinstitutionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('institutions', function (Blueprint $table) {
+            //columns
+            $table->id();
+            $table->string('name', 85);
+            $table->unsignedBigInteger('city_id');
+            $table->timestamps();
+
+            //relationships
+            $table->foreign('city_id')->references('id')->on('cities')
+                ->cascadeOnDelete()
+                ->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('institutions');
+    }
+}
