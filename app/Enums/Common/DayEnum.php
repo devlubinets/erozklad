88bf11c96d7fd867208385126f8afea3bf68eab1
ad <dev.lubinets@gmail.com>
2022-04-10 15:51:58 +0300

    feat(enum): add enum
    *Timetable
    *Day

---
+++ app/Enums/Common/DayEnum.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Enums\Common;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static OptionOne()
+ * @method static static OptionTwo()
+ * @method static static OptionThree()
+ */
+final class DayEnum extends Enum
+{
+    const MONDAY = 'monday';
+    const TUESDAY = 'tuesday';
+    const WEDNESDAY = 'wednesday';
+    const THURSDAY = 'thursday';
+    const FRIDAY = 'friday';
+    const SATURDAY = 'saturday';
+    const SUNDAY = 'sunday';
+}
